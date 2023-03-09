454ba614b1ad30032a83139488268d71a4240bc7
Julian <43455717+Bluju@users.noreply.github.com>
2023-03-09 12:45:45 -0600

    Update FibSequence309.cpp

--- FibSequence309.cpp
+++ FibSequence309.cpp
@@ -1,11 +1,29 @@
 #include <bits/stdc++.h>
 
+// The Fibonacci Sequence is 0,1,1,2,3,5,8
+// Write a program that has the following functions: 
+
 int main(){
 
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
+  //What is the max Fibonacci number that is less than the max integer that can be represented in C++
 
 }
 
 int maxFib(int upperBound){
+
+}
+
+int indexOfFib(int fibNum){
+
+}
+
+int fibRecursion(int index){
+
+}
+
+double fibUsingFormula(int index){
 
 }
