38029d4e966c15a3e900956d3288b0eff9c75aed
Bluju <JASahagun26@gmail.com>
2023-03-12 19:23:57 -0500

    Finished implementing all of the functions and main()

--- FibSequence309.cpp
+++ FibSequence309.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cassert> //remove after testing
+#include <chrono>
+#include <math.h>
 
 // The Fibonacci Sequence is 0,1,1,2,3,5,8
 // F0 = 0, F1 = 1, and Fn = Fn-1 + Fn-2
@@ -12,18 +15,30 @@ int main(){
   double fibUsingFormula(int index);
   
   //1. What is the max Fibonacci number that is less than the max integer that can be represented in C++? (call maxFib)
+  auto start = chrono::steady_clock::now();
   int largestFib = maxFib(INT_MAX);
-  cout << "The maximum Fibonacci number less than the max integer is: " << largestFib << "\n";
+  auto end = chrono::steady_clock::now();
+  chrono::duration<double> timeUsingMaxFib = end - start;
+  cout << "1) The maximum Fibonacci number less than the max integer is: " << largestFib << "\n";
+
   //2. What is the index of the above number? (call indexOfFib)
-  //int indexLargest = indexOfFib(largestFib);
-  int index5 = indexOfFib(5);
-  cout << "Index of 5: " << index5 << "\n";
+  int indexLargest = indexOfFib(largestFib);
+  cout << "2) The index of the largest Fibonacci number less than the max integer is: " << indexLargest << "\n";
+      //what if the number given is not in the fibonacci sequence?
+  
   //3. How much time does it take to find the number in 1) ? use the appropriate methods from the library
+  cout << "3) Time to find the max fib in 1) : " << timeUsingMaxFib.count() << "s\n";
 
   //4. How much time does it take to find the number by calling fibRecursion and using the index found in 2) as the parameter?
-  cout << fibRecursion(11) << "\n";
-  //5. What is the value if you call fibUsingFormula with the index found in 2)?
+  start = chrono::steady_clock::now();
+  fibRecursion(indexLargest);
+  end = chrono::steady_clock::now();
+  chrono::duration<double> timeUsingFibRecursion = end - start;
+  cout << "4) Time to find the max fib in 1) using recursion: " << timeUsingFibRecursion.count() << "s\n"; 
 
+  //5. What is the value if you call fibUsingFormula with the index found in 2)?
+  double largestWithFormula = fibUsingFormula(indexLargest);
+  cout << "5) Value using Binet's formula with the index found in 2) : " << largestWithFormula << "\n";
 }
 
 int maxFib(int upperBound){
@@ -38,7 +53,7 @@ int maxFib(int upperBound){
   while(fib < upperBound && fib > 0){
     fibPrev = fib;
     fib = fibPrev + fibPrevPrev;
-    cout << fib << "\t" << fibPrev << "\t" << fibPrevPrev << "\n";
+    //cout << fib << "\t" << fibPrev << "\t" << fibPrevPrev << "\n";
     fibPrevPrev = fibPrev; 
     
   }
@@ -50,20 +65,37 @@ int maxFib(int upperBound){
 
 int indexOfFib(int fibNum){
   //returns the index of fibNum which is a number in the Fibonacci sequence. The index starts with 0, so the index of 2 is 3.
-
+  int index = 0;
+  if(fibNum == 0){
+    return 0;
+  }
+  index++;
+  int fib = 1;          //Fn
+  int fibPrev = 1;      //Fn-1
+  int fibPrevPrev = 0;  //Fn-2
+  while(fib < fibNum && fib > 0){
+    fibPrev = fib;
+    fib = fibPrev + fibPrevPrev;
+    fibPrevPrev = fibPrev;
+    index++;
+  }
+  if(fib == fibNum){
+    return index;
+  }
+  cout << "The number given, " << fibNum << ", is not a number in the Fibonacci sequence.\n";
+  return -1;
 }
 
 int fibRecursion(int index){
   //returns the number in the Fibonacci sequence with the given index. The function should be written using recursion
-  if(index == 0)
+  if(index == 0){
     return 0;
+  }
   return index <= 2? 1 : fibRecursion(index - 1) + fibRecursion(index - 2);
 
 }
 
 double fibUsingFormula(int index){
-  //returns the number in the Fibonacci sequence with the given index, the computation is using the following formula:
-  // Fn = [(1+sqrt(5)^n / 2^n) - (1-sqrt(5))^n / 2^n] / sqrt(5)
-
-
+  //returns the number in the Fibonacci sequence with the given index, using Binet's formula
+  return (pow((1+sqrt(5))/2,index) - pow((1-sqrt(5))/2,index)) / sqrt(5);
 }
